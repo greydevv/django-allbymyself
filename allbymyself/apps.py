bb97592e3cf038548e84d6e53ab905cb4a2fe023
Greyson Murray <greyson.murray@gmail.com>
2021-07-02 05:18:01 -0400

    fix 'name' attr

--- allbymyself/apps.py
+++ allbymyself/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AllByMyselfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.allbymyself'
+    name = 'allbymyself'
